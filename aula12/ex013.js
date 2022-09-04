f460e9b0ae9926a455cc675007df0d9bf5eb42fe
vinicius <93842439+viniciuspra@users.noreply.github.com>
2022-09-04 17:38:16 -0300

    Create ex013.js

---
+++ aula12/ex013.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var dia = agora.getDay()
+
+switch(dia) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break
+}
