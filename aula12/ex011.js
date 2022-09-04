072788d9bae9e10dc0a5f269f883ab165cb0ac1c
vinicius <93842439+viniciuspra@users.noreply.github.com>
2022-09-04 17:16:16 -0300

    Create ex011.js

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 66
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade >= 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
+
+ 
