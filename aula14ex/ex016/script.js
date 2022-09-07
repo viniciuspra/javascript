1ec344ec514d4b1473631615a5a4a70cf2b19291
vinicius <93842439+viniciuspra@users.noreply.github.com>
2022-09-07 16:11:39 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,16 +1,19 @@
 function contar() {
-    var inum = document.getElementById('inum')
-    var fnum = document.getElementById('fnum')
-    var pass = document.getElementById('pass')
-    var res = document.getElementById('res')
-    res.innerHTML = ''
+    let inum = document.getElementById('inum')
+    let fnum = document.getElementById('fnum')
+    let pass = document.getElementById('pass')
+    let res = document.getElementById('res')
     if (inum.value.length == 0 || fnum.value.length == 0 || pass.value.length == 0) {
         res.innerHTML = 'Impossivel contar! Tente Novamente'
     } else {
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inum.value)
         let f = Number(fnum.value)
         let p = Number(pass.value)
-        
+        if (p <= 0) {
+            alert('[ERRO] Passo inválido! Considerando Passo = 1 ')
+            p = 1
+        }
         if (i < f) {
             for (let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} &#x27A1; `
@@ -18,8 +21,9 @@ function contar() {
             res.innerHTML += '&#x1F6A9'
         } else {
             for (let c = i; c >= f; c -= p) {
-                res.innerHTML -= `${c} &#x27A1; `
+                res.innerHTML += `${c} &#x27A1; `
             }
             res.innerHTML += '&#x1F6A9'
         }
     }
+}
