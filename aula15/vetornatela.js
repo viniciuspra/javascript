38c1afd5dbb2715ad2841d8de0ca454d94ff77cb
vinicius <93842439+viniciuspra@users.noreply.github.com>
2022-09-07 18:22:35 -0300

    Update vetornatela.js

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,6 +1,16 @@
-let num = [4,7,1,9,2,0]
+let num = [4,7,1,9,2]
 num.sort()
+num.push(3) 
+console.log(num)
+let p = num.indexOf(9)
+console.log(`(${p})`)
 
+/*
 for (let c = 0; c < num.length; c++) {
     console.log(`${c+1} -> ${num[c]}`)
 } 
+
+for (let c in num) {
+    console.log(num[c])
+}
+*/
