ad94f7893dc5974a408db0bc81161a993b912070
vinicius <93842439+viniciuspra@users.noreply.github.com>
2022-09-14 22:46:47 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -5,9 +5,9 @@ let array = []
 
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
-        return true 
+        return true
     } else {
-        return false
+        return false 
     }
 }
 
@@ -19,13 +19,44 @@ function inLista(n, l) {
     }
 }
 
+
 function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, array)) { 
-        array.push()
+    if (isNumero(num.value) && !inLista(num.value, array)) {
+        array.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
-    }else {
-        alert('Valor invalido ou já encontrado na lista')
+        tab.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Valor inválido ou já encontrado na lista. ')
     }
+    txtnum.value = ''
+    txtnum.focus()
+}
 
+function finalizar() {
+    if (array.length == 0) {
+        alert('Valores não informado')
+    } else {
+        let tot = array.length
+        let maior = array[0]
+        let menor = array[0]
+        let media = 0
+        let soma = 0
+        for(let pos in array) {
+            soma += array[pos]
+            if (array[pos] > maior) 
+                maior = array[pos]
+            if (array[pos] < menor)
+                menor = array[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números atualizados</p>`
+        res.innerHTML += `<p>O MAIOR valor informado é ${maior}.</p>`
+        res.innerHTML += `<p>O MENOR valor informado é ${menor}.</p>`
+        res.innerHTML += `<p>A soma de todos o valores é ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores é ${media}.</p>`
+
+    }
 }
